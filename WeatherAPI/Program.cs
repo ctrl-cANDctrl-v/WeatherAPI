8401a0a51d73ea01b477cd10d1cf74372d110e38
azaza_vrik <azaza_vrik@mail.ru>
2023-02-09 22:00:37 +0300

    Добавьте файлы проекта.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using WeatherAPI;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var apiKey = "de652bea5fc39e262e384811c44b47a7";
+var client = new HttpClient();
+Console.WriteLine("Введите город");
+var cityUser = Console.ReadLine();
+var response2 = await client.GetAsync(
+    $"https://api.openweathermap.org/data/2.5/forecast?q={cityUser}&appid={apiKey}&units=metric&lang=ru");
+
+if (response2.IsSuccessStatusCode)
+{  
+    DateTime doWeek;
+    int u = 0;
+    var result2 = await response2.Content.ReadAsStringAsync();
+    var model = JsonConvert.DeserializeObject<WeatherFor4Days>(result2);
+    Console.WriteLine($"{model.city.name}, {model.city.country}, {DateTime.Now}\n" +
+            $"Погодные условия: {model.list[0].weather[0].description}\n" +
+            $"Температура воздуха: {model.list[0].main.temp}°С, ощущается как {model.list[0].main.feels_like}°С\n" +
+            $"Скорость ветра: {model.list[0].wind.speed}м/с\n" +
+            $"Давление: {model.list[0].main.grnd_level} мм рт. ст.\n" +
+            $"Влажность: {model.list[0].main.humidity}%" +
+            $"--------Прогноз на 4 дня----------\n");
+    Console.WriteLine("\tДата и день     \t | Макс и мин. * \t | Описание");
+     for (int i = 1; i < 5; i++){
+        Console.WriteLine($"\t{DateTime.Parse(model.list[u].dt_txt).ToShortDateString()}, {DateTime.Parse(model.list[u].dt_txt).DayOfWeek} \t | {model.list[i].main.temp_min}, {model.list[i].main.temp_max} \t | {model.list[i].weather[0].description}");
+        u += 8;}
+}
+
+
---
+++ WeatherAPI/WeatherFor4Days.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI
+{
+    public class WeatherFor4Days
+    {
+        public string cod { get; set; }
+        public int message { get; set; }
+        public int cnt { get; set; }
+        public List[] list { get; set; }
+        public City city { get; set; }
+    }
+
+    public class City
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord1 coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Coord1
+    {
+        public float lat { get; set; }
+        public float lon { get; set; }
+    }
+
+    public class List
+    {
+        public int dt { get; set; }
+        public Main1 main { get; set; }
+        public Weather1[] weather { get; set; }
+        public Clouds1 clouds { get; set; }
+        public Wind1 wind { get; set; }
+        public int visibility { get; set; }
+        public float pop { get; set; }
+        public Sys1 sys { get; set; }
+        public string dt_txt { get; set; }
+        public Snow snow { get; set; }
+    }
+
+    public class Main1
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+        public int pressure { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+        public int humidity { get; set; }
+        public float temp_kf { get; set; }
+    }
+
+    public class Clouds1
+    {
+        public int all { get; set; }
+    }
+
+    public class Wind1
+    {
+        public float speed { get; set; }
+        public int deg { get; set; }
+        public float gust { get; set; }
+    }
+
+    public class Sys1
+    {
+        public string pod { get; set; }
+    }
+
+    public class Snow
+    {
+        public float _3h { get; set; }
+    }
+
+    public class Weather1
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+}
